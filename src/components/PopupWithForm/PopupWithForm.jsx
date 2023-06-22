9255d943219de034da6513b5bbd5be2446caeec3
Gvozdenko Sergey <exetto@gmail.com>
2023-06-22 11:18:58 +0300

    fix

--- src/components/PopupWithForm/PopupWithForm.jsx
+++ src/components/PopupWithForm/PopupWithForm.jsx
@@ -1,15 +1,15 @@
-export default function PopupWithForm({name, title, Button, children, open, onClose}) {
+export default function PopupWithForm({name, title, button, children, open, onClose}) {
   return (
     <div className={`popup popup_type_${name} ${open && "popup_opened"}`}>
       <div className="popup__container">
         <button type="button" className="popup__close" aria-label="Закрыть" onClick={onClose}/>
         <h2 className="popup__title">{title}</h2>
-        <form className="popup__form" name="userName" noValidate="">
+        <form className="popup__form" name={name} noValidate="">
           {children}
           <button
             type="submit"
             className="button popup__save popup__save_enabled popup__save_disabled">
-            {Button||"Сохранить"}
+            {button||"Сохранить"}
           </button>
         </form>
       </div>
